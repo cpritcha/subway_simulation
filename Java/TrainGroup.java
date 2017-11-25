2bedd4a4a25c10458eb800d75418ef28d7bf3ad4
jboydst <jacob.boydston@asu.edu>
2017-11-25 09:13:24 -0700

    Added a TrainGroup class.
    This is a coupled class to hold the trains for a given loop. I think the idea will be for each loop to have its own TrainGroup, Scheduler, and StationGroup.

---
+++ Java/TrainGroup.java
@@ -0,0 +1,46 @@
+package Subway;
+import view.modeling.ViewableDigraph;
+import java.util.ArrayList;
+
+public class TrainGroup extends ViewableDigraph {
+	
+	ArrayList<Train> trains;
+	
+	public TrainGroup(String name, ArrayList<String> TrainNames) {
+		super(name);
+		
+		// Instantiate each of the train names
+		trains = new ArrayList<Train>(TrainNames.size());
+		for (String tname : TrainNames) {
+			trains.add(new Train(tname));
+		}
+		
+		// Add the inports
+		addInport(Train.IN_PASSENGER_LOAD_PORT);
+		addInport(Train.IN_BREAKDOWN_PORT);
+		addInport(Train.IN_MOVE_RESPONSE_PORT);
+		
+		// Add the outports
+		addOutport(Train.OUT_PASSENGER_UNLOAD_PORT);
+		addOutport(Train.OUT_REQUEST_MOVE_PORT);
+		addOutport(Train.OUT_REQUEST_MOVE_PORT);
+		
+		// Couple all of the trains to the in and out ports
+		for (Train train : trains) {
+			// Add the train
+			add(train);
+			
+			// Inports
+			addCoupling(this,Train.IN_PASSENGER_LOAD_PORT,train,Train.IN_PASSENGER_LOAD_PORT);
+			addCoupling(this,Train.IN_BREAKDOWN_PORT,train,Train.IN_BREAKDOWN_PORT);
+			addCoupling(this,Train.IN_MOVE_RESPONSE_PORT,train,Train.IN_MOVE_RESPONSE_PORT);
+			
+			// Outports
+			addCoupling(train,Train.OUT_PASSENGER_UNLOAD_PORT,this,Train.OUT_PASSENGER_UNLOAD_PORT);
+			addCoupling(train,Train.OUT_REQUEST_MOVE_PORT,this,Train.OUT_REQUEST_MOVE_PORT);
+			addCoupling(train,Train.OUT_REQUEST_MOVE_PORT,this,Train.OUT_REQUEST_MOVE_PORT);
+		}
+		
+		initialize();
+	}
+}
