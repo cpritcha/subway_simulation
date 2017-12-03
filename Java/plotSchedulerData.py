04cd0b2cbe7a0ef34ee594c841aaf314ea66673b
jboydst <jacob.boydston@asu.edu>
2017-12-03 11:11:45 -0700

    Plot Script now shows station names on y-axis.

--- Java/plotSchedulerData.py
+++ Java/plotSchedulerData.py
@@ -2,6 +2,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter, MaxNLocator
+
+xs = [0,2,4,6,8,10]
+labels = {0:'Kennedy',2:'Lawrence East',4:'Ellesmere',6:'Midland',8:'Scarborough Centre',10:'McCowan'}
+
+def format_yticks(tick_val,tick_pos):
+    if int(tick_val) in xs:
+        return labels[int(tick_val)]
+    else: 
+        return ''
 
 data = np.genfromtxt('..\..\Scheduler_Scarborough.log',skip_header=1,dtype=None)
 
@@ -10,8 +20,8 @@ names = data['f1']
 locationData = data['f2']
 
 # alter the location numbers above 12 to count back down
-indices = np.where(locationData>12)
-locationData[indices] = 24-locationData[indices]
+indices = np.where(locationData>10)
+locationData[indices] = 22-locationData[indices]
 
 # Convert the names to strings
 names = np.array([n.decode('utf-8') for n in names])
@@ -36,4 +46,6 @@ for name in uniqueNames:
     plt.grid(True)
     plt.xlabel('Time (minutes)')
 
+plt.gca().yaxis.set_major_formatter(FuncFormatter(format_yticks))
+plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
 plt.show()
